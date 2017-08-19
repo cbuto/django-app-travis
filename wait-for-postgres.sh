a04dd0dd37417d62c8a74e1a378dcb749ec1532c
Casey Buto <cbuto22@gmail.com>
2017-08-19 12:16:16 -0700

    Fix

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -3,7 +3,6 @@
 postgres_host=$1
 postgres_port=$2
 shift 2
-cmd="$@"
 
 # wait for the postgres docker to be running
 while ! pg_isready -h $postgres_host -p $postgres_port -q -U postgres; do
@@ -11,7 +10,4 @@ while ! pg_isready -h $postgres_host -p $postgres_port -q -U postgres; do
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
-
-# run the command
-exec $cmd
+>&2 echo "Postgres is up"
